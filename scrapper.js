f380f94d01004896be8959dcf6b066e38d45351c
Withee Poositasai <witheep@gmail.com>
2017-10-28 02:14:53 +0700

    Scrapper able to get query result and link

--- scrapper.js
+++ scrapper.js
@@ -4,7 +4,19 @@ let q = 'slice';
 let topic = 'js';
 
 scrapeIt('https://developer.mozilla.org/en-US/search?q='+q+'&topic='+topic, {
-    data:  ".result-list-item > h4 > a"
+    result:  {
+        listItem: "ul.result-list > li",
+        
+        data: {
+            name: {
+                selector: "div.result-list-item > h4 > a"
+            },
+            link: {
+                selector: "div.result-list-item > h4 > a",
+                attr: "href"
+            }
+        }
+    }
     
 }).then(page => {
     console.log(page)
