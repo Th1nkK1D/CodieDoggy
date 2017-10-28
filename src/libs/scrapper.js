290075caad9fd8588b990522a69dfdd925d89b7a
Withee Poositasai <witheep@gmail.com>
2017-10-28 12:11:12 +0700

    Re structure scraper fo muti-lang support

--- src/libs/scrapper.js
+++ src/libs/scrapper.js
@@ -4,39 +4,76 @@ let q = 'slice';
 let topic = 'js';
 let type = 'syntax';
 
-// Scrape Query
-export async function getQuery(q,topic) {
-    const res = await scrapeIt('https://developer.mozilla.org/en-US/search?q='+q+'&topic='+topic, {
-        resultList:  {
+async function scrapeMdnQuery(lang,q,n) {
+    const res = await scrapeIt('https://developer.mozilla.org/en-US/search?q='+q+'&topic='+lang, {
+        list:  {
             listItem: "ul.result-list > li",
             
             data: {
                 name: {
                     selector: "div.result-list-item > h4 > a"
                 },
-                link: {
+                url: {
                     selector: "div.result-list-item > h4 > a",
                     attr: "href"
                 }
             }
         }
-        
     })
-    return res;
+    return res.list.slice(0,n);
 }
 
-// Scrape Content
-export function getContent(type,ur) {    
+async function scrapeMdnContent(type,url) {
+    let res;
+
     if(type === 'syntax') {
         // Scrap Syntax
-        scrapeIt(url, {
+        res = await scrapeIt(url, {
             name: "div.document-title > h1",
             syntax: "pre.syntaxbox > code"
-        }).then(result => {
-            console.log(result);
         })
     } else if(type === 'example') {
         // Example
 
     }
+
+    return res;
+}
+
+// Scrape Query
+export async function getQuery(lang,q,n) {
+    let res;
+
+    if(lang === 'react') {
+        // fetch react
+    } if(lang === 'js') {
+        // fetch MDN
+        res = await scrapeMdnQuery(lang,q,n);
+    } else {
+        res = {
+            'error': true,
+            'message': lang+' is not supported',
+        }
+    }
+
+    return res;
+}
+
+// Scrape Content
+export async function getContent(lang,type,url) {  
+    let res;
+
+    if(lang === 'react') {
+        // fetch react
+    } if(lang === 'js') {
+        // fetch MDN
+        res = await scrapeMdnContent(type,url);
+    } else {
+        res = {
+            'error': true,
+            'message': lang+' is not supported',
+        }
+    }
+
+    return res;
 }
--- src/testmodule.js
+++ src/testmodule.js
@@ -1,6 +1,9 @@
-import * as scrapper from './scrapper'; 
+import * as scrapper from './libs/scrapper'; 
 
 (async () => {
-    let result = await scrapper.getQuery('slice','js');
-    console.log(result);
+    const list = await scrapper.getQuery('js','slice',3);
+    console.log(list);
+
+    const content = await scrapper.getContent('js','syntax',list[0].url);   
+    console.log(content);
 })();
