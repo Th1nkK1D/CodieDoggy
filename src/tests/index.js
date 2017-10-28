f57133e248d0deece4dcf38b72eeaddec127ad9d
Jirachai Chansivanon <antronic.inc@gmail.com>
2017-10-28 13:59:31 +0700

    merge scaper operation

--- src/tests/index.js
+++ src/tests/index.js
@@ -10,7 +10,19 @@ const params = process.argv.splice(2).join(' ');
 
 const needs = analyzeNeed(params);
 
+let res;
+getQuery(needs.msg.language, needs.msg.query, 3)
+  .then((result) => {
+    res = result;
+  })
+  .catch((err) => {
+    res = err;
+  })
 
-getQuery(needs.msg.language, needs.msg.query, 3).then((result ) => console.log(result))
+getContent(needs.msg.language, needs.msg.mode, 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray/slice')
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => console.log(err))
 
 // console.log();
